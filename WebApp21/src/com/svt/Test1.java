633575d2ecffb9f6ea8719d0ef2996340d8d6bda
onpul <chmj072@gmail.com>
2022-04-28 16:47:51 +0900

    20220428

--- WebApp21/src/com/svt/Test1.java
+++ WebApp21/src/com/svt/Test1.java
@@ -7,6 +7,8 @@ package com.svt;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Test1 extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
+	// Test1 클래스 이름에 오류 뜨면 커서 대서 첫번 째 거 클릭
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
@@ -30,6 +33,8 @@ public class Test1 extends HttpServlet
 	
 	protected void doGetPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
+		// ① 
+		/*
 		String name = "홍길동";
 		String age = "19";
 		
@@ -43,6 +48,65 @@ public class Test1 extends HttpServlet
 		pw.println("<body>");
 		pw.println("<h1>name : " + name + "</h1>");
 		pw.println("<h1>age : " + age + "</h1>");
+		pw.println("</body>");
+		pw.println("</html>");
+		*/
+		// 테트리스 팩을 만들어 배포하면 테트리스밖에 못한다.
+		
+		
+		// ②
+		
+		// 『ServletConfig』
+		//-- 컨테이너가 서블릿을 초기화할 때
+		//   서블릿 당 하나씩 ServletConfig 을 생성하게 된다.
+		ServletConfig config = getServletConfig();
+		
+		// 『web.xml』에 『<init-param>』
+		String name = config.getInitParameter("name");
+		String age = config.getInitParameter("age");
+		
+		/*
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		
+		pw.println("<html>");
+		pw.println("<head>");
+		pw.println("<title>" + "Test1.java" + "</title>");
+		pw.println("</head>");
+		pw.println("<body>");
+		pw.println("<h1>name : " + name + "</h1>");
+		pw.println("<h1>age : " + age + "</h1>");
+		pw.println("</body>");
+		pw.println("</html>");
+		*/
+		// 배포 후에도 값을 변경할 수 있다. web.xml에서!
+		// 테트리스 팩 안에 칩셋을 변형하여 다른 테마의 테트리스도 할 수 있고...
+		
+		
+		// ③
+		// 『ServletContext』
+		//-- ServletConfig 는 서블릿에 대한 환경을 설정하는 과정에서
+		//   필요한 값들을 전달하는 형태로 주로 사용된다면
+		//   ServletContext 는 서블릿에서 사용되는 컨텍스트를
+		//   구성하는 형태로 활용된다.
+		ServletContext context = getServletContext();
+		
+		// 『web.xml』에 『<context-param>』 사용 가능
+		String type = context.getInitParameter("type");
+		
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		
+		pw.println("<html>");
+		pw.println("<head>");
+		pw.println("<title>" + "Test1.java" + "</title>");
+		pw.println("</head>");
+		pw.println("<body>");
+		pw.println("<h1>name : " + name + "</h1>");
+		pw.println("<h1>age : " + age + "</h1>");
+		
+		pw.println("<h1>type : " + type + "</h1>");
+		
 		pw.println("</body>");
 		pw.println("</html>");
 	}
